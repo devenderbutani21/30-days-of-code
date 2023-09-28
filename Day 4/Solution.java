f5efbc672d85715ad1e3d635bbc50390ba494d43
devenderbutani21 <devenderbutani21@gmail.com>
2023-09-28 11:11:19 -0400

    Day 4 Code

---
+++ Day 4/Solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public String decodeAtIndex(String s, int k) {
+        long size = 0;
+        int n = s.length();
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int digit = Character.getNumericValue(c);
+                size *= digit;
+            } else {
+                size++;
+            }
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            
+            if (Character.isDigit(c)) {
+                int digit = Character.getNumericValue(c);
+                size /= digit;
+                k %= size;
+            } else {
+                if (k == 0 || k == size) {
+                    result.insert(0, c);
+                    return result.toString();
+                }
+                size--; 
+            }
+        }
+        return ""; 
+    }
+}
