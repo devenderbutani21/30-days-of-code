c9b5c1ccf4f58edc3f67865d0840b6977dd36aa2
devenderbutani21 <devenderbutani21@gmail.com>
2023-09-29 10:30:24 -0400

    Day 5 of Code

---
+++ Day 5/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+            while (start < mid && nums[start] == nums[mid]) {
+                start++;
+            }
+
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
