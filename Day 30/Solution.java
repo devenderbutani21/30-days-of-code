1076adbfa6e2b6b45e7a48350ca6dd23012a5756
devenderbutani21 <devenderbutani21@gmail.com>
2023-10-24 09:42:57 -0400

    Day 30 of Code

---
+++ Day 30/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    if (root == null) {
+        return result;
+    }
+    
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+        int levelSize = queue.size();
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < levelSize; i++) {
+            TreeNode node = queue.poll();
+            max = Math.max(max, node.val);
+            
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+
+        result.add(max);
+    }
+
+    return result;
+}
+}
